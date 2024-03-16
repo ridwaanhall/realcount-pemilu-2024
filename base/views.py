255993844671d083c3e46b99851d60c439dbeb7c
ridwaanhall <erbyl14@gmail.com>
2024-03-16 23:59:05 +0700

    Simple App🚀

---
+++ base/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+
+# Create your views here.
+
+rooms = [
+    {'id': 1, 'name': 'Room ridwan'},
+    {'id': 2, 'name': 'Room hafidhah'},
+    {'id': 3, 'name': 'Room afkariana'},
+]
+
+def home(request):
+    # return HttpResponse("ridwaanhall")
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    # return HttpResponse("room ridwaanhall")
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
