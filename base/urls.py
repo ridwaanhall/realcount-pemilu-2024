7d52d8e2e1b41f36d0d89a19b39b44b0b654404a
ridwaanhall <erbyl14@gmail.com>
2024-03-20 21:03:08 +0700

    Area Level 5 - Desa/Kelurahan ✅

--- base/urls.py
+++ base/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('area/<str:area_code_lv2>/', views.level2, name='level2'),
     path('area/<str:area_code_lv2>/<str:area_code_lv3>/', views.level3, name='level3'),
     path('area/<str:area_code_lv2>/<str:area_code_lv3>/<str:area_code_lv4>/', views.level4, name='level4'),
+    path('area/<str:area_code_lv2>/<str:area_code_lv3>/<str:area_code_lv4>/<str:area_code_lv5>/', views.level5, name='level5'),
     # path('test', views.cek_doang, name='home')
 ]
--- base/views.py
+++ base/views.py
@@ -46,7 +46,16 @@ def level4(request, area_code_lv2, area_code_lv3, area_code_lv4):
         error_message = f"Failed to fetch data from {url}. Status code: {response.status_code}"
         return render(request, 'base/error.html', {'error_message': error_message})
 
-
+def level5(request, area_code_lv2, area_code_lv3, area_code_lv4, area_code_lv5):
+    url = f'http://127.0.0.1:8000/api/level-api/{area_code_lv2}/{area_code_lv3}/{area_code_lv4}/{area_code_lv5}/'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        context = response.json()
+        return render(request, 'base/level5.html', context)
+    else:
+        error_message = f"Failed to fetch data from {url}. Status code: {response.status_code}"
+        return render(request, 'base/error.html', {'error_message': error_message})
 
 def cek_doang(request):
     pass
