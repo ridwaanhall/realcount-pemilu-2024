104565f233ff3013dcb37658c67f9e474e6c9139
ridwaanhall <erbyl14@gmail.com>
2024-03-26 13:26:21 +0700

    Update API Route

--- base/api/urls.py
+++ base/api/urls.py
@@ -12,18 +12,6 @@ urlpatterns = [
     path('wilayah/<str:wilayah_tingkat2>/<str:wilayah_tingkat3>/', views.WilayahAPIView.as_view(), name='wilayah_tingkat_3'),
     path('wilayah/<str:wilayah_tingkat2>/<str:wilayah_tingkat3>/<str:wilayah_tingkat4>/', views.WilayahAPIView.as_view(), name='wilayah_tingkat_4'),
     path('wilayah/<str:wilayah_tingkat2>/<str:wilayah_tingkat3>/<str:wilayah_tingkat4>/<str:wilayah_tingkat5>/', views.WilayahAPIView.as_view(), name='wilayah_tingkat_5'),
-    
-    # path('wilayah/<str:wilayah_tingkat2>/', views.WilayahTingkat2.as_view(), name='wilayah_tingkat_2'),
-    # path('wilayah/<str:wilayah_tingkat2>/<str:wilayah_tingkat3>/', views.WilayahTingkat3.as_view(), name='wilayah_tingkat_3'),
-    # path('wilayah/<str:wilayah_tingkat2>/<str:wilayah_tingkat3>/<str:wilayah_tingkat4>/', views.WilayahTingkat4.as_view(), name='wilayah_tingkat_4'),
-    # path('wilayah/<str:wilayah_tingkat2>/<str:wilayah_tingkat3>/<str:wilayah_tingkat4>/<str:wilayah_tingkat5>/', views.WilayahTingkat5.as_view(), name='wilayah_tingkat_5'),
-
-    # path('votes/', views.VotesTingkat1.as_view(), name='votes_tingkat_1'),
-    # path('votes/<str:votes_tingkat2>/', views.VotesTingkat2.as_view(), name='votes_tingkat_2'),
-    # path('votes/<str:votes_tingkat2>/<str:votes_tingkat3>/', views.VotesTingkat3.as_view(), name='votes_tingkat_3'),
-    # path('votes/<str:votes_tingkat2>/<str:votes_tingkat3>/<str:votes_tingkat4>/', views.VotesTingkat4.as_view(), name='votes_tingkat_4'),
-    # path('votes/<str:votes_tingkat2>/<str:votes_tingkat3>/<str:votes_tingkat4>/<str:votes_tingkat5>/', views.VotesTingkat5.as_view(), name='votes_tingkat_5'),
-    # path('votes/<str:votes_tingkat2>/<str:votes_tingkat3>/<str:votes_tingkat4>/<str:votes_tingkat5>/<str:votes_tingkat6>/', views.VotesTingkat6.as_view(), name='votes_tingkat_6'),
 
     path('votes/', views.VotesAPIView.as_view(), name='votes_tingkat_1'),
     path('votes/<str:votes_tingkat2>/', views.VotesAPIView.as_view(), name='votes_tingkat_2'),
