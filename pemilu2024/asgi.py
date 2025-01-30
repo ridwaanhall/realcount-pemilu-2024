53559b56a79822b159a2cfa2532c48da9335960c
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-01-30 14:52:03 +0700

    Refactor ASGI and WSGI applications; update STATICFILES_DIRS and set STATIC_ROOT

--- pemilu2024/asgi.py
+++ pemilu2024/asgi.py
@@ -14,3 +14,5 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pemilu2024.settings')
 
 application = get_asgi_application()
+
+app = get_asgi_application()
